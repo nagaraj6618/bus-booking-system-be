7c48249b38d1bc994f9913700165c07c30ea0746
NAGARAJ S <nagaraj516700@gmail.com>
2024-06-20 22:17:21 +0530

    Created Ticket Model

--- models/ticketModel.js
+++ models/ticketModel.js
@@ -3,12 +3,37 @@ const mongoose = require("mongoose");
 
 const ticketSchema = mongoose.Schema(
    {
-      passengerId:{
-         type:String,
+      passengerIds:{
+         type:[String],
          required:true
       },
+      from:{
+         type:String,
+         required:true,
+      },
+      to:{
+         type:String,
+         required:true,
+      },
+      bookedAt:{
+         type:String,
+         required:true,
+      },
+      numberOfSeat:{
+         type:Number,
+         required:true,
+      },
+      Price:{
+         type:Number,
+         required:true
+      },
+      seatNumber:{
+         type:[String],
+         required:true
+      }
 
    },{
       timestamps:true
    }
 ) 
+module.exports = mongoose.model('ticket',ticketSchema);
