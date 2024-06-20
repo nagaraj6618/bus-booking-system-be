04cf739c8bf508834e80ba29c7606fa932167801
NAGARAJ S <nagaraj516700@gmail.com>
2024-06-20 09:24:54 +0530

    Db connect function included and added middlewares

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
-
+const mongoose = require('mongoose');
+const cors = require('cors');
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 const MONGODB_URL = process.env.MONGODB_URL; 
 
 // console.log(PORT,MONGODB_URL);
+//middleware 
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors({origin:true,credentials:true}));
+
+//Db Connection function
+(async function (){
+   try{
+      const connection = await mongoose.connect(MONGODB_URL);
+      // console.log(connection);
+      if(connection){
+         console.log('DB Connected..')
+      }
+   }
+   catch(error){
+      console.log('Db Connection Failed:\n',error);
+   }
+})()
 
 //Checking ..
 app.get('/api/v1',(req,res) => {
