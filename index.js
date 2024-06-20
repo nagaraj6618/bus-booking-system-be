d454a405c2154dfd7ba87cd536ecb36cafa1edfd
NAGARAJ S <nagaraj516700@gmail.com>
2024-06-20 08:59:38 +0530

    Added get method for checking

--- index.js
+++ index.js
@@ -8,4 +8,11 @@ const MONGODB_URL = process.env.MONGODB_URL;
 
 // console.log(PORT,MONGODB_URL);
 
+//Checking ..
+app.get('/api/v1',(req,res) => {
+   res.status(200).json({
+      message:"Server Working",status:true
+   })
+})
+
 app.listen(PORT,()=>console.log(`Server running at port: ${PORT}`));
