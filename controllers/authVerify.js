3d434ee12ca28e5df99c14a41119e5f5e99cb1b3
NAGARAJ S <nagaraj516700@gmail.com>
2024-06-20 22:48:04 +0530

    Admin authentication middleware created

--- controllers/authVerify.js
+++ controllers/authVerify.js
@@ -1,10 +1,34 @@
-
+const jwt = require('jsonwebtoken')
 
 function verifyToken(token){
+   let reqUser = null;
+   
+   if(!token){
+      return null;
+   }
+   const accessToken = token.split('Bearer')[1].trim();
+
+   jwt.verify(accessToken,process.env.JSON_WEBTOKEN_KEY, (err,user) => {
+      if(err){
+         return null;
+      }
+      reqUser = user;
+   })
+   return reqUser;
 
 }
 function verifyAdmin(req,res,next){
    
+   const token = req.headers.authorization
+   const user = verifyToken(token);
+   console.log(user)
+   if(!user || user.role !== "admin"){
+      return res.status(401).json({
+         message:"You are not authorized.",
+         success:true,
+      })
+   }
+   next();
 }
 
 function verifyUser(req,res,next){
