1683d5bae90d850cd6782ee0e76681fc13e30be5
NAGARAJ S <nagaraj516700@gmail.com>
2024-06-20 12:03:10 +0530

    Login and Register Created

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,9 @@
 const userModel = require('../models/userModel');
 const bcryptjs = require("bcryptjs");
+const jsonWebToken = require('jsonwebtoken');
 async function register(req,res){
-   console.log(req.body);
-   // const data =[];
+   // console.log(req.body);
+
    const {name,userName,email,password,} = req.body;
    if (!name || !userName || !email || !password) {
       return res.status(400).json({
@@ -56,11 +57,69 @@ async function register(req,res){
 }
 async function login(req,res){
    console.log(req.body);
-   const data = [];
-   res.status(200).json({
-      message:"User login successfull",
-      status:true,
-      data:data,
-   })
+   // const data = [];
+   const {userNameOrEmail,password} = req.body;
+   if(!userNameOrEmail || !password){
+      return res.status(400).json({
+         message: "Username or email or password required",
+         status: false
+      });
+   }
+   try{
+      const findUser = await userModel.findOne({
+         $or:[
+            {
+               email:userNameOrEmail
+            },
+            {
+               userName:userNameOrEmail
+            }
+         ]
+      });
+      if(!findUser){
+         return res.status(404).json({
+            message:"User not found please register the account.",
+            status:false
+         })
+      }
+      // console.log(findUser);
+      const isPassordCorrect = await bcryptjs.compareSync(password,findUser.password); 
+      if(!isPassordCorrect){
+         return res.status(400).json({
+            message:"Invalid Passowrd",
+            success:false,
+         })
+      }
+      //Generate TOken for the user
+      const token = jsonWebToken.sign(
+         {
+            id:findUser._id,
+            role:findUser.role
+         },
+         process.env.JSON_WEBTOKEN_KEY,
+         {
+            expiresIn:"1h"
+         }
+      );
+      
+      res.cookie("accessToken",token,{
+         expires:new Date(Date.now() + 3600000),//One hour
+         httpOnly:true,
+         secure:true,
+      }).status(200).json({
+         message:"User login successfull",
+         status:true,
+         data:findUser,
+         token:token,
+      })
+   }
+   catch(error){
+      res.status(500).json({
+         message:error.message,
+         status:false,
+         error:error
+      })
+   }
+   
 }
 module.exports = {register,login};
