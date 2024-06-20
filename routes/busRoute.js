f571180226b6ca9cff298e72fc1cbb01fa2a8599
NAGARAJ S <nagaraj516700@gmail.com>
2024-06-20 23:01:13 +0530

    Updated Busroute

--- routes/busRoute.js
+++ routes/busRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { getAllBus, addNewBus, getBusById, updateBusDetails, deleteBusById } = require('../controllers/busController');
+const { verifyAdmin } = require('../controllers/authVerify');
 const router = express.Router();
 
-router.route('/').get(getAllBus).post(addNewBus);
-router.route('/:id').get(getBusById).put(updateBusDetails).delete(deleteBusById);
+router.route('/').get(getAllBus).post(verifyAdmin,addNewBus);
+router.route('/:id').get(getBusById).put(verifyAdmin,updateBusDetails).delete(verifyAdmin,deleteBusById);
 
 module.exports = router;
